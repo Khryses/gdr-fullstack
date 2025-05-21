e5f1229e95740a4b4a51bc90ea89b914d5d4c8cf
Khryses <alice.golfari@gmail.com>
2025-05-21 21:59:32 +0200

    🧽 rimozione backend per reinserimento pulito

--- backend/src/routes/diceRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { authMiddleware } = require('../middlewares/authMiddleware');
-const diceController = require('../controllers/diceController');
-
-router.post('/dice/roll', authMiddleware, diceController.rollDice);
-
-module.exports = router;
--- backend/src/routes/logRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { authMiddleware, isMaster } = require('../middlewares/authMiddleware');
-const logController = require('../controllers/logMasterController');
-
-router.post('/log/azione', authMiddleware, isMaster, logController.logAzione);
-
-module.exports = router;
